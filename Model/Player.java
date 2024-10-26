1b1a7536f56470a0b425872dc477c0e53a59fd55
CHIKITIN19 <141179159+CHIKITIN19@users.noreply.github.com>
2024-10-26 16:40:50 -0600

    Se crea la clase Player para el juego Othello

--- Model/Player.java
+++ Model/Player.java
@@ -9,5 +9,26 @@ package Model;
  * @author 9567
  */
 public class Player {
+    private int piece;
+    private String colors;
+    private String name;
     
+    public Player(String colors, String name){
+        this.name= name;
+        this.colors=colors;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColors() {
+        return colors;
+    }
+    
+    public void setName(String name){
+        if(name != null && !name.isEmpty()){
+            this.name= name;
+        }
+    }
 }
