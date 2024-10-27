e60e097e0ba1bf2c028b7f3bae93d74035fa6015
CHIKITIN19 <141179159+CHIKITIN19@users.noreply.github.com>
2024-10-27 14:33:31 -0600

    Se terminan los metodos de la clase board o llamado Tablero

--- Model/Board.java
+++ Model/Board.java
@@ -57,9 +57,10 @@ public class Board {
     public void setCurrentPlayer(Player CurrentPlayer) {
         this.CurrentPlayer = CurrentPlayer;
     }
+    
     //Caso base si sale del tablero, encuentra una posicion vacia
     //O simplemente no encuentra un rodeo, termina 
-    private boolean Movements(int row, int column, int deltaRow, int deltaColumn, String playerColor, boolean flipPiece){
+    private boolean ValidateCapture(int row, int column, int deltaRow, int deltaColumn, String playerColor, boolean flipPiece){
         int j = row + deltaRow;
         int k = column + deltaColumn;
         
@@ -69,6 +70,94 @@ public class Board {
         if(board[j][k].getColors().equals(playerColor)){
             return flipPiece;
         }
-        return Movements(j,k, deltaRow, deltaColumn, playerColor, true);
+        return ValidateCapture(j,k, deltaRow, deltaColumn, playerColor, true);
     }
+    public int PossibleMovents(String playerColor){
+        int countMovements = 0;
+        if (!CurrentPlayer.getColors().equals(playerColor)) {
+            return 0;
+        }
+        for (int j = 0; j < 12; j++) {
+            for (int k = 0; k < 12; k++) {
+                if (ValidMovement(j,k)) {
+                    countMovements++;
+                }
+            }
+        }
+        return countMovements;
+    }
+    public boolean ValidMovement (int row, int column){
+        if(board[row][column] != null){
+            return false;
+        }
+        for(int j = -1; j<=1; j++){
+            for(int k = -1; k<=1; k++){
+                if(j == 0 && j == 0){
+                    continue;
+                }
+                if(ValidateCapture(row, column, j, k, CurrentPlayer.getColors(), true)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public void MakeMovements(int row, int column, int deltaRow, int deltaColumn,String playerColor){
+        int newRow = row + deltaRow;
+        int newColumn = column + deltaColumn;
+        
+        while(newRow >=0 && newRow<12 && newColumn>= 0 && newColumn<12 && board[newRow][newColumn]
+                != null && !board[newRow][newColumn].getColors().equals(playerColor)){
+             
+            board[newRow][newColumn].FlipColors();
+            newRow += deltaRow;
+            newColumn += deltaColumn;
+        }
+    }
+    public void PlacePiece(int row, int column){
+        if(ValidMovement(row, column)){
+            board[row][column] = new Piece(CurrentPlayer.getColors());
+        }
+        for(int j = -1 ; j<=1; j++){
+            for(int k = -1; k<=1; k++){
+                if(j==0 && k == 0){
+                    continue;
+                }
+                if(ValidateCapture(row, column, j, k, CurrentPlayer.getColors(), false)){
+                    MakeMovements(row, column, j, k, CurrentPlayer.getColors());
+                }
+            }
+            ChangeShift();
+        }
+    }
+    public void ChangeShift(){
+        if(CurrentPlayer == player1){
+            CurrentPlayer = player2;
+        }else{
+            CurrentPlayer = player1;
+        }
+    }
+    public int PlayerPiece1(){
+        int count1 = 0;
+        for (int j = 0; j < 12; j++) {
+            for (int k = 0; k < 12; k++) {
+                if(board[j][k] != null && board[j][k].getColors().equals(player1.getColors())){
+                    count1++;
+                }
+            }
+        }
+        return count1;
+    }
+   public int PlayerPiece2(){
+       int count2 = 0;
+       for (int j = 0; j < 12; j++) {
+           for (int k = 0; k < 12; k++) {
+               if(board[j][k] != null && board[j][k].getColors().equals(player2.getColors())){
+                   count2++;
+               }
+           }
+       }
+       return count2;
+   }
+   
 }
