e952715fd79605f79b627fc52d3630f6370cfbc9
SebasVba <sebastianbarquerocr@gmail.com>
2024-11-02 15:54:16 -0600

    Se crean Varios Comandos
    Se definen los comandos, pero no están programados

---
+++ Model/Command/UpdateBoardCommand.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Command;
+
+import Model.Board;
+
+/**
+ *
+ * @author sebas
+ */
+public class UpdateBoardCommand extends BoardCommand {
+
+    public UpdateBoardCommand(Board board) {
+        super(board);
+    }
+
+    @Override
+    public void execute() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
